0e03f7240eb06c885848bcf33b4b2a24a41b70bc
Hassan Jbara <hassan.jbara@exxeta.com>
2024-05-05 21:34:19 +0200

    update python package

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
     version="1.0", 
     packages=find_packages(), 
     install_requires=open("requirements.txt", "r", encoding="utf-8").read().splitlines(),
+    include_package_data=True,
 )
