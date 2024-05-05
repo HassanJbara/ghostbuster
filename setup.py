fe8fb2252ab675294d2ba36c4cc2116c68ea0546
Hassan Jbara <hassan.jbara@exxeta.com>
2024-05-05 21:53:36 +0200

    fix python package

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version="1.0", 
     packages=find_packages(), 
     install_requires=open("requirements.txt", "r", encoding="utf-8").read().splitlines(),
+    packages=['ghostbuster','ghostbuster.model'],
+    package_data={'ghostbuster.model': ['*']},
     include_package_data=True,
 )
