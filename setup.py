673873052d3d35643346d9ba860893a7cda9ef75
Hassan Jbara <hassan.jbara@exxeta.com>
2024-05-05 21:54:13 +0200

    fix python package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Ghostbuster", 
     version="1.0", 
-    packages=find_packages(), 
+    # packages=find_packages(), 
     install_requires=open("requirements.txt", "r", encoding="utf-8").read().splitlines(),
     packages=['ghostbuster','ghostbuster.model'],
     package_data={'ghostbuster.model': ['*']},
